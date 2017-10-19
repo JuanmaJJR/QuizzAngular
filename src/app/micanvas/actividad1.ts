1ab9b48cc2ee8b14c15e4d4bb26e4e1ecee1e3b0
JuanmaJJR <juan.jusue@live-u.tad.com>
2017-10-19 14:11:35 +0200

    Añadido listeners y nueva view

--- src/app/micanvas/actividad1.ts
+++ src/app/micanvas/actividad1.ts
@@ -4,9 +4,11 @@ import {DataHolder} from '../milib/dataholder/dataholder';
 import {Motor} from '../milib/engines/motor';
 import {Imagen} from '../milib/views/imgs/imagen';
 import {Window} from '../milib/views/window/Window';
-import {Button} from '../milib/views/buttons/button';
+import {Button,ButtonListener} from '../milib/views/buttons/button';
+import {Label} from '../milib/views/labels/label';
 
-export class Actividad1 implements EventsAdminListener{
+
+export class Actividad1 implements EventsAdminListener,ButtonListener{
 
     private motor:Motor;
     private panelMenu:Panel;
@@ -17,6 +19,18 @@ export class Actividad1 implements EventsAdminListener{
     private buttonNuevo:Button;
     private buttonContinuar:Button;
     private imagenCentro:Imagen;
+    private w2:Window;
+    private pmw=DataHolder.instance.nScreenWidth*0.6;
+    private pmx=DataHolder.instance.nScreenWidth2-(this.pmw>>1);
+    private pmh=DataHolder.instance.nScreenHeight*0.6;
+    private pmy=DataHolder.instance.nScreenHeight2-(this.pmh>>1);
+    private lblPreg:Label;
+    private buttonResp1:Button;
+    private buttonResp2:Button;
+    private buttonResp3:Button;
+    private buttonResp4:Button;
+    
+
 
     constructor(vMotor:Motor){
         this.motor=vMotor;
@@ -24,7 +38,6 @@ export class Actividad1 implements EventsAdminListener{
         this.imagenFondo.setImg('./assets/1.jpg');
         this.motor.setRaiz(this.imagenFondo);
         this.crearEscenarioMenu();
-        
     }
 
     /**
@@ -33,17 +46,11 @@ export class Actividad1 implements EventsAdminListener{
      */
     private crearEscenarioMenu():void{
 
-        let pmw=DataHolder.instance.nScreenWidth*0.6;
-        let pmh=DataHolder.instance.nScreenHeight*0.6;
-        let pmx=DataHolder.instance.nScreenWidth2-(pmw>>1);
-        let pmy=DataHolder.instance.nScreenHeight2-(pmh>>1);
-     
-        
-        this.window1=new Window(this.motor,pmx,pmy,pmw,pmh);
+        this.window1=new Window(this.motor,this.pmx,this.pmy,this.pmw,this.pmh);
         this.motor.addViewToParentView(this.imagenFondo,this.window1);
 
         //CREAMOS IMAGEN Y AÑADIMOS AL WINDOW CENTRAL
-       this.imagenCentro=new Imagen(this.motor,0,0,pmw,pmh);
+       this.imagenCentro=new Imagen(this.motor,0,0,this.pmw,this.pmh);
        this.imagenCentro.setImg('./assets/quiz.jpg');
        this.motor.addViewToParentView(this.window1,this.imagenCentro);
         
@@ -53,18 +60,64 @@ export class Actividad1 implements EventsAdminListener{
        this.buttonSalir.setTexto("SALIR");
        this.motor.addViewToParentView(this.window1,this.buttonSalir);
        this.buttonSalir.setImagePath('./assets/btn.png')
+       this.buttonSalir.setListener(this);
+       
        //AÑADIR BOTON CONTINUAR
        this.buttonContinuar=new Button(this.motor,340,180,200,100);
        this.buttonContinuar.setTexto("CONTINUAR");
        this.motor.addViewToParentView(this.window1,this.buttonContinuar);
-       this.buttonContinuar.setImagePath('./assets/btn.png')
+       this.buttonContinuar.setImagePath('./assets/btn.png');
+       this.buttonContinuar.setListener(this);
        //AÑADIR BOTON NUEVO
        this.buttonNuevo=new Button(this.motor,340,40,200,100);
        this.buttonNuevo.setTexto("NUEVO");
        this.motor.addViewToParentView(this.window1,this.buttonNuevo);
-       this.buttonNuevo.setImagePath('./assets/btn.png')
+       this.buttonNuevo.setImagePath('./assets/btn.png');
+       this.buttonNuevo.setListener(this);
+       
+       
+
 
     }
+    buttonListenerOnClick?(btn:Button):void{
+        if(btn==this.buttonNuevo){
+            //CREAMOS WINDOW NUEVO
+            this.w2=new Window(this.motor,this.pmx,this.pmy,this.pmw,this.pmh);
+            this.motor.addViewToParentView(this.imagenFondo,this.w2);
+            //CREAMOS LABEL DE LA PREGUNTA
+            this.lblPreg= new Label(this.motor,340,40,200,100);
+            this.lblPreg.setTexto("PREGUNTA 1 LOCO");
+            this.motor.addViewToParentView(this.w2,this.lblPreg);
+            //CREAMOS BOTON RESPUESTA 1
+            this.buttonResp1 = new Button (this.motor,120,220,200,100);
+            this.buttonResp1.setTexto("RESPUESTA 1");
+            this.motor.addViewToParentView(this.w2,this.buttonResp1);
+            //CREAMOS BOTON RESPUESTA 2
+            this.buttonResp2 = new Button (this.motor,520,220,200,100);
+            this.buttonResp2.setTexto("RESPUESTA 2");
+            this.motor.addViewToParentView(this.w2,this.buttonResp2);
+            //CREAMOS BOTON RESPUESTA 3
+            this.buttonResp3 = new Button (this.motor,120,320,200,100);
+            this.buttonResp3.setTexto("RESPUESTA 3");
+            this.motor.addViewToParentView(this.w2,this.buttonResp3);
+            //CREAMOS BOTON RESPUESTA 4
+            this.buttonResp4 = new Button (this.motor,520,320,200,100);
+            this.buttonResp4.setTexto("RESPUESTA 4");
+            this.motor.addViewToParentView(this.w2,this.buttonResp4);
+            
+        }
+        if(btn==this.buttonContinuar){
+            this.buttonNuevo.setTexto("ADIOS");
+        }
+        if(btn==this.buttonSalir){
+            this.buttonNuevo.setTexto("ADIOS");
+        }
+        
+
+    }
+
+    
+    
 
     private crearEscenarioJuego():void{
         
